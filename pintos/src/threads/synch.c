fe4d145a024cb0eacc1b64806a117ed6eb73f061
Eros Smith <eros@cs.utexas.edu>
2014-02-20 19:07:00 -0600

    added thread yield in lock release

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -265,7 +265,7 @@ lock_release (struct lock *lock)
   }
   lock->holder = NULL;
   sema_up (&lock->semaphore);
-  //thread_yield();
+  thread_yield();
 }
 
 /* Returns true if the current thread holds LOCK, false
